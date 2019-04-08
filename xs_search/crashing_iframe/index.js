228859cd332ff26efdc6b6f373d897e08c2b0f3d
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2019-04-08 21:05:01 +0200

    Tunak Tune

--- xs_search/crashing_iframe/index.js
+++ xs_search/crashing_iframe/index.js
@@ -85,7 +85,7 @@ async function tune_for_url(url, initial_size) {
     while (!(50 < measurement && measurement < 100)) {
         console.log(measurement);
         let delta = measurement - 75;
-        size += delta*1*MB;
+        size += delta*512*KB;
         measurement = await get_it(url, size);
     }
     console.log(measurement);
